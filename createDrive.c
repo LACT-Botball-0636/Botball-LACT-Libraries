d7e7e36422b9be29693cb58404e8c269dd768370
James <jameshe424@gmail.com>
2015-07-26 18:48:15 -0700

    Mega Ultra Changes
    Changed the drive commands and wait commands to return an int showing if
    it successfully completes. Added several useful constants, and started
    working on create_wait_angle. Made sure the create driving functions
    worked. Added A TON of comments

--- createDrive.c
+++ createDrive.c
@@ -3,11 +3,23 @@
 
 //Create information: cable download speed of 14.0625 kilobytes/s.
 //More info at http://www.irobot.com/~/media/MainSite/PDFs/About/STEM/Create/create_2_Open_Interface_Spec.pdf
+//Important notes: Avoid calling create_read_block directly. Using it incorrectly leads to bad things, but the library calls it safely.
 
 //Get the second to rightmost byte.
 #define get_high_byte2(a) (((a)>>8)&255)
 //Get the rightmost byte.
-#define get_low_byte2(a) ((a)&255)
+#define get_low_byte2(a) ((a)&255)'
+
+//Constants. Don't change these
+#define TWO_BYTES 65536
+#define PI 3.14159265358979
+//FIND VALUES
+#define MMToEncoder ???
+#define EncoderToMM 1/MMToEncoder
+
+//Adjustable values. You can change these
+#define ENCODER_HIGH_LOOP 60000
+#define ENCODER_LOW_LOOP TWO_BYTES-1-ENCODER_HIGH_LOOP
 
 /*
 Sends two bytes to the Create by from the rightmost two bytes of an integer.
@@ -32,6 +44,7 @@ int create_read_signed(int bytes)
 	create_read_block(pointer,bytes);
 	int total = 0;
 	int place = pow(256,bytes);
+	int i;
 	//summing the bytes here
 	for(i=0;i<bytes;i++)
 	{
@@ -52,6 +65,7 @@ int create_read_unsigned(int bytes)
 	create_read_block(pointer,bytes);
 	int total = 0;
 	int place = pow(256,bytes);
+	int i;
 	//summing the bytes here
 	for(i=0;i<bytes;i++)
 	{
@@ -61,45 +75,108 @@ int create_read_unsigned(int bytes)
 	return total;
 }
 //Driving. Yay, encoder fun. 
+/*
+Waits until the Create has travelled a certain distance.
+Returns 1 if the robot has successfully stopped by travelling a certain distance. Returns -1 if an irregular stop has occurred.
+dist: the distance that will be travelled. Reached when average of right and left encoder has passed the dist.
 
-void create_wait_dist(int dist)//dist is in mm
+TODO: Add a piece of code to stop the program if the robot has not moved. Write the actual code
+*/
+int create_wait_dist(int dist)//dist is in mm
 {
-	create_write_byte(156);
-	create_write_int(dist);
+	//Int vel = read velocity (signed)
+	if(vel==0)
+	{
+		printf("Error, cannot wait dist if robot isn't moving");
+		return -1;
+	}
+	//Int initPos,currPos = read position (signed)
+	int target = (initPos + dist*MMToEncoder)%TWO_BYTES
+	int reps = (initPos+dist*MMToEncoder)/TWO_BYTES;
+	while(reps>0)
+	{
+		//currPos = read position(signed);
+		if(vel<0&&currPos)
+		reps--;
+		/*if(not moving)
+		{
+			printf("Error, Create stopped moving");
+			return -1;
+		}*/
+	}
 }
-void create_wait_angle(int angle)//degrees, negative = right
+//Most likely gonna borrow from the drive.c black magic wizardry to write this. Doing this later
+/*
+Waits until the create turns a certain angle.
+Returns 1 if the robot has successfully stopped by turning a certain distance. Returns -1 if an irregular stop has occurred.
+angle: the angle to be travelled.
+*/
+int create_wait_angle(int angle)//degrees, negative = right
 {
+	//Int vel = read velocity (signed)
+	if(vel==0)
+	{
+		printf("Error, cannot wait angle if robot isn't moving");
+		return -1;
+	}
 	create_write_byte(157);
 	create_write_int(angle);
 }
+//Not sure if usable any more, will check later
 void create_wait_event(int event)//see #defines for possible answers.  Use 255-event for the inverse
 {
 	create_write_byte(158);
 	create_write_byte(event);
 }
-
-void create_drive_direct_dist(int r_speed, int l_speed, int dist)
+/*
+Drives moving the left and right motors at a certain speed. Ends when it reaches a distance. Returns an int showing if it drove correctly
+r_speed/l_speed: speed of the left and right motors.
+dist: distance it drives until.
+*/
+int create_drive_direct_dist(int r_speed, int l_speed, int dist)
 {
 	create_write_byte(145);
 	create_write_int(r_speed);
 	create_write_int(l_speed);
-	create_wait_dist(dist);
+	int success = create_wait_dist(dist);
+	create_stop();
+	return success;
 }
-void create_drive_direct_left(int r_speed, int l_speed, int angle)
+/*
+Drives moving the left and right motors at a certain speed. Ends when it reaches an angle
+r_speed/l_speed: speed of the left and right motors.
+angle: angle it drives until.
+*/
+int create_drive_direct_left(int r_speed, int l_speed, int angle)
 {
 	create_write_byte(145);
 	create_write_int(r_speed);
 	create_write_int(l_speed);
-	create_wait_angle(angle);
+	int success = create_wait_angle(angle);
+	create_stop();
+	return success;
 }
-void create_drive_direct_right(int r_speed, int l_speed, int angle)
+/*
+Drives moving the left and right motors at a certain speed. Ends when it reaches an angle
+r_speed/l_speed: speed of the left and right motors.
+angle: angle it drives until. -angle means right turn
+*/
+int create_drive_direct_right(int r_speed, int l_speed, int angle)
 {
 	create_write_byte(145);
 	create_write_int(r_speed);
 	create_write_int(l_speed);
-	create_wait_angle(-angle);
+	int success = create_wait_angle(-angle);
+	create_stop();
+	return success;
 }
-void create_right(int angle, int radius, int speed)
+/*
+Turns right at a certain angle, radius, and speed.
+angle: the angle to turn, calculated in degrees.
+radius: the radius of the turn, PROBABLY in MM
+speed: the speed to turn. Range of 0-500. For negative values use create_left.
+*/
+int create_right(int angle, int radius, int speed)
 {
 	create_write_byte(137);
 	create_write_int(speed);
@@ -108,9 +185,17 @@ void create_right(int angle, int radius, int speed)
 	}else{
 		create_write_int(-radius);
 	}
-	create_wait_angle(-angle);
+	int success = create_wait_angle(-angle);
+	create_stop();
+	return success;
 }
-void create_left(int angle, int radius, int speed)
+/*
+Turns left at a certain angle, radius, and speed.
+angle: the angle to turn, calculated in degrees.
+radius: the radius of the turn, PROBABLY in MM
+speed: the speed to turn. Range of 0-500. For negative values use create_right.
+*/
+int create_left(int angle, int radius, int speed)
 {
 	create_write_byte(137);
 	create_write_int(speed);
@@ -119,24 +204,40 @@ void create_left(int angle, int radius, int speed)
 	}else{
 		create_write_int(radius);
 	}
-	create_wait_angle(angle);
+	int success = create_wait_angle(angle);
+	create_stop();
+	return success;
 }
-void create_forward(int dist, int speed)
+/*
+Drives forward a certain distance. Returns an int showing if it drove properly.
+dist: the distance to travel
+speed: the speed to travel at
+*/
+int create_forward(int dist, int speed)
 {
 	create_write_byte(145);
 	create_write_int(speed);
 	create_write_int(speed);
-	create_wait_dist(dist);
+	int success = create_wait_dist(dist);
+	create_stop();
+	return success;
 }
-void create_backward(int dist, int speed)
+/*
+Drives backward a certain distance. Returns an int showing if it drove properly
+dist: the distance to travel
+speed: the speed to travel at
+*/
+int create_backward(int dist, int speed)
 {
 	create_write_byte(145);
 	create_write_int(-speed);
 	create_write_int(-speed);
-	create_wait_dist(-dist);
+	int success = create_wait_dist(-dist);
+	create_stop();
+	return success;
 }
 
-//Not driving stuff. Hopefully this remains the same
+//Functions unrelated to driving
 
 //Reboots the Create
 void create_crash()
@@ -153,8 +254,8 @@ void create_send()
 }
 
 //Receives one byte from the Create and does nothing with it.
-void create_recieve(){
-	
+void create_recieve()
+{
 	char buffer[1];
 	char *bptr = buffer;
 	create_read_block(bptr,1);
@@ -168,7 +269,7 @@ void create_block()
 	create_recieve();
 }
 
-//so long as we are connected to the controller, no reason to use the direct motor command instead of this one. 
+//Runs the create vacuum at speed
 void create_motors(int speed)//speed is from 0 to 128 inclusive
 {
 	create_write_byte(144);
--- createDrive.h
+++ createDrive.h
@@ -1,4 +1,5 @@
 //Documentation in the createDrive.c file in order to keep this file concise and easier to use for quick reference.
+//Todo list: Constants, create_wait_angle, create_wait_dist, check create_wait_event
 #ifndef _FINALCREATE
 #define _FINALCREATE
 
@@ -12,22 +13,24 @@
 #define BUTTON_PLAY 17//TODO: finish all events.  p16 of create docs
 #define SEN_0 18
 
-//Try to avoid calling this and create_write_byte outside of the libraries. Left in here for convenience.
+//Try to avoid calling this and create_write_byte outside of the libraries. Left in here for convenience though Same goes for byte reading functions.
 void create_write_int(int integer);
-
+int create_read_signed(int bytes);
+int create_read_unsigned(int bytes);
+	
 //There is no longer scripting with the introduction of the create 2, but these will function similarly to how they worked without scripting.
-void create_wait_dist(int dist);//dist is in mm
-void create_wait_angle(int angle);//degrees, negative = right
+int create_wait_dist(int dist);//dist is in mm
+int create_wait_angle(int angle);//degrees, negative = right
 void create_wait_event(int event);//see #defines for possible answers.  Use 255-event for the inverse
 
 //Drive commands
-void create_drive_direct_dist(int r_speed, int l_speed, int dist);
-void create_drive_direct_left(int r_speed, int l_speed, int angle);
-void create_drive_direct_right(int r_speed, int l_speed, int angle);
-void create_right(int angle, int radius, int speed);
-void create_left(int angle, int radius, int speed);
-void create_forward(int dist, int speed);
-void create_backward(int dist, int speed);
+int create_drive_direct_dist(int r_speed, int l_speed, int dist);
+int create_drive_direct_left(int r_speed, int l_speed, int angle);
+int create_drive_direct_right(int r_speed, int l_speed, int angle);
+int create_right(int angle, int radius, int speed);
+int create_left(int angle, int radius, int speed);
+int create_forward(int dist, int speed);
+int create_backward(int dist, int speed);
 
 //Not Driving commands
 void create_crash();
