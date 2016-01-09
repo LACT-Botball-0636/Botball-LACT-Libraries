79e217e43fca27389fef775119d959e7dafb15d3
christopherdumas <christopherdumas@me.com>
2016-01-09 09:39:47 -0800

    Refactor newmenu.h, newmenu.c, generic.h, generic.c, drive.h, drive.c, and createDrive.c

--- drive.h
+++ drive.h
@@ -1,29 +1,28 @@
-
 #ifndef _DRIVE_H_
 #define _DRIVE_H_
 
-#define gmpc(port) get_motor_position_counter(port)
+#define GMPC(port) get_motor_position_counter(port)
 
 
-//primary driving code
-#define MOT_LEFT 0//Polyp edition! Unique to each robot
-#define MOT_RIGHT 2 //Unique to each robot
+// Primary driving code
+#define MOT_LEFT 0 // Polyp edition! Unique to each robot
+#define MOT_RIGHT 2 // Unique to each robot
 #define PI 3.14159265358979
 
-#define SPD 100//turning
-#define SPDl 97//left forward
-#define SPDr 98//right forward
+#define SPD 100 // Turning
+#define SPDl 97 // Left forward
+#define SPDr 98 // Right forward
 #define rdistmult 1.0
-#define SPDlb 92.//left backward
-#define SPDrb 96.//right backward
-#define rdistmultb (SPDrb/SPDlb)
-#define wheeldiameter 5.3 //Unique to each robot
-#define ks 14.5 //Unique to each robot
-#define CMtoBEMF (850/(PI*wheeldiameter))
+#define SPDlb 92. // Left backward
+#define SPDrb 96. // Right backward
+#define rdistmultb (SPDrb / SPDlb)
+#define wheeldiameter 5.3 // Unique to each robot
+#define ks 14.5 // Unique to each robot
+#define CMtoBEMF (850 / (PI * wheeldiameter))
 
 void drive_off();
 void clear_all_drive();
-void drive(int mL,int mR);
+void drive(int mL, int mR);
 
 
 void right(float degrees, float radius);
--- newmenu.h
+++ newmenu.h
@@ -5,12 +5,14 @@
 #define DEFAULT_OPTION 0
 extern int currstate;//defined in generic.h
 
-struct menuitem{
-	int snum;
-	char* name;
+struct menuitem {
+  int snum;
+  char* name;
 };
+
 extern struct menuitem menu[];
-void Get_Mode();
-//other functions are hidden- just use Get_Mode();
+
+void get_mode();
+//other functions are hidden- just use get_mode();
 
 #endif
