c5a989eb4c993a4cd88a3211410531f61b80d0e7
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-03-03 20:18:04 -0800

    Added Single sensor line following pt2

--- createDriveBetter.h
+++ createDriveBetter.h
@@ -6,6 +6,15 @@
 
 #define lfCliff get_create_lfcliff_amt
 #define rfCliff get_create_rfcliff_amt
+#define lCliff get_create_lcliff_amt
+#define rCliff get_create_rcliff_amt
+
+/* Line follow constants for single sensor line following
+ * LEFT means follow on the left side of the line
+ * RIGHT means follow on the right side of the line
+ */
+#define LEFT 0
+#define RIGHT 1
 
 /* Gyro Constants
  * GYRO defines the orientation of the wallaby
@@ -14,9 +23,11 @@
  */
 #define GYRO $ // 1: x, 2: y, 3: z
 #define GYRO_SENS 15 // Usually a value of 15 works quite well for the create. Make this value higher to make it less sensitive.
-float gyro_dev; // Use the function calc_dev() to set this variable
+float gyro_dev; // Use the function calc_dev() to set this variable 
 
-void create_line_follow(int dist, int speed);
+void create_line_follow_center(int dist, int speed);
+void create_line_follow_left(int dist, int speed, int side);
+void create_line_follow_right(int dist, int speed, int side);
 void create_forward(int dist, int speed);
 void create_backward(int dist, int speed);
 void create_left(int degrees, int speed);
@@ -26,5 +37,6 @@ void create_forward_gyro(float dist, int speed);
 void calc_dev();
 
 void create_setup_cliff();
-int leftWhite, rightWhite;
-int leftBlack, rightBlack;
+void create_skip_cliff();
+int lfWhite, lWhite, rfWhite, rWhite;
+int lfBlack, lBlack, rfBlack, rBlack;
