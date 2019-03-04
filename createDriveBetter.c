f8adbe4106870f32249ac8d89d559a577a976935
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-03-03 20:17:12 -0800

    Added single sensor line following

--- createDriveBetter.c
+++ createDriveBetter.c
@@ -1,28 +1,28 @@
 #include "createDrive.h"
 #include <math.h>
 /**
- * create_line_follow makes the create line follow by using the built-in cliff sensors. 
- * create_setup_cliff must be run before create_line_follow will work
+ * create_line_follow_center makes the create line follow by using the built-in front cliff sensors. 
+ * create_setup_cliff must be run before create_line_follow_center will work
  * Requires following to be defined:
- * leftBlack and leftWhite are the readings for the left cliff sensor on the create
- * rightBlack and rightWhite are the readings for the right cliff sensor on the create
+ * lfBlack and lfWhite are the readings for the left front cliff sensor on the create
+ * rfBlack and rfWhite are the readings for the right front cliff sensor on the create
  * dist: distance to travel in centimeters
  * speed: speed to drive at on a range from 0-1000
  */
-void create_line_follow(int dist, int speed)
+void create_line_follow_center(int dist, int speed)
 {
-  //create_line_follow makes the create line follow for "dist" centimeters at "speed" speed
+  //create_line_follow_center makes the create line follow for "dist" centimeters at "speed" speed
   
   dist*=10; //Converts centimeters into millimeters
   set_create_distance(0); //Clears create position counter
   create_drive_straight(speed); //Initializes movement
   while(abs(get_create_distance())<abs(dist)) //while distance has not been achieved, perform the following:
   {
-    if(lfCliff() < (leftBlack+leftWhite)/2)				  //if the left tophat sensor sees black
+    if(lfCliff() < (lfBlack+lfWhite)/2)				  //if the left tophat sensor sees black
     {
       create_drive_direct(speed*.75,speed*1.25); //drive to the left
     }
-    else if(rfCliff() < (rightBlack+rightWhite)/2)		  //if the right tophat sensor sees black
+    else if(rfCliff() < (rfBlack+rfWhite)/2)		  //if the right tophat sensor sees black
     {
       create_drive_direct(speed*1.25,speed*.75); //drive to the right
     }
@@ -34,6 +34,57 @@ void create_line_follow(int dist, int speed)
   }
   create_stop();							  //stop the create once distance has been achieved
 }
+
+/**
+ * create_line_follow_left makes the create line follow by using the built-in left cliff sensor. 
+ * create_setup_cliff must be run before create_line_follow_right will work
+ * Requires following to be defined:
+ * lBlack and lWhite are the readings for the left cliff sensor on the create
+ * dist: distance to travel in centimeters
+ * speed: speed to drive at on a range from 0-1000
+ */
+void create_line_follow_left(int dist, int speed, int side) {
+	dist*=10; //Convert centimeters to millimeters
+    speed = 100*speed/1000;
+    set_create_distance(0);
+    
+    while(abs(get_create_distance()) < abs(dist)) {
+        
+        float sens = 0.01;
+        if (side == RIGHT) {
+        	create_drive_direct(sens*speed*(lWhite - lCliff()), sens*speed*(lCliff() - lBlack));
+        } else if (side == LEFT) {
+        	create_drive_direct(sens*speed*(lCliff() - lBlack), sens*speed*(lWhite - lCliff()));
+        }
+    }
+    create_stop();
+}
+
+/**
+ * create_line_follow_right makes the create line follow by using the built-in right cliff sensor. 
+ * create_setup_cliff must be run before create_line_follow_right will work
+ * Requires following to be defined:
+ * rBlack and rWhite are the readings for the right cliff sensor on the create
+ * dist: distance to travel in centimeters
+ * speed: speed to drive at on a range from 0-1000
+ */
+void create_line_follow_right(int dist, int speed, int side) {
+	dist*=10; //Convert centimeters to millimeters
+    speed = 100*speed/1000;
+    set_create_distance(0);
+    
+    while(abs(get_create_distance()) < abs(dist)) {
+        
+        float sens = 0.01;
+        if (side == RIGHT) {
+        	create_drive_direct(sens*speed*(rWhite - rCliff()), sens*speed*(rCliff() - rBlack));
+        } else if (side == LEFT) {
+        	create_drive_direct(sens*speed*(rCliff() - rBlack), sens*speed*(rWhite - rCliff()));
+        }
+    }
+    create_stop();
+}
+
 /**
  * create_forward makes the create go forward straight. 
  * Requires no predefined constants
@@ -149,18 +200,20 @@ void calc_dev() {
 
 /**
  * create_setup_cliff calibrates the cliff sensors on the create for black and white lines
- * Requires leftWhite, rightWhite, leftBlack, rightBlack to be global variables
+ * Requires lfWhite, rfWhite, lWhite, rWhite, lfBlack, rfBlack, lBlack, rBlack to be global variables
  */
 void create_setup_cliff() 
 {
     int accept = 0;
     while (!accept) 
     {
-        printf("\n\n------------------------\n\nMove cliff sensors over white area of board.\nPress right button to set.\n");
+        printf("\n\n------------------------\n\nMove the ENTIRE create over white area of board.\nPress right button to set.\n");
         while (!right_button()) {}
-        leftWhite = lfCliff();
-        rightWhite = rfCliff();
-        printf("Left cliff value: %d, Right cliff value: %d\n", leftWhite, rightWhite);
+        lfWhite = lfCliff();
+        lWhite = lCliff();
+        rfWhite = rfCliff();
+        rWhite = rCliff();
+        printf("Left front cliff value: %d\nLeft cliff value: %d\nRight front cliff value: %d\nRight cliff value: %d\n", lfWhite, lWhite, rfWhite, rWhite);
         msleep(1000);
         printf("Press right button to accept, left button to reject.\n");
         while(!right_button() && !left_button()) {}
@@ -174,11 +227,45 @@ void create_setup_cliff()
     accept = 0;
     while (!accept) 
     {
-        printf("\n\n------------------------\n\nMove cliff sensors over black area of board.\nPress right button to set.\n");
+        printf("\n\n------------------------\n\nMove FRONT cliff sensors over black area of board.\nPress right button to set.\n");
         while (!right_button()) {}
-        leftBlack = lfCliff();
-        rightBlack = rfCliff();
-        printf("Left cliff value: %d, Right cliff value: %d\n", leftBlack, rightBlack);
+        lfBlack = lfCliff();
+        rfBlack = rfCliff();
+        printf("Left front cliff value: %d\nRight front cliff value: %d\n", lfBlack, rfBlack);
+        msleep(1000);
+        printf("Press right button to accept, left button to reject.\n");
+        while(!right_button() && !left_button()) {}
+        if (right_button()) 
+        {
+            accept = 1;
+        }
+    }
+    
+    msleep(1000);
+    accept = 0;
+    while (!accept) 
+    {
+        printf("\n\n------------------------\n\nMove LEFT cliff sensor over black area of board.\nPress right button to set.\n");
+        while (!right_button()) {}
+        lBlack = lCliff();
+        printf("Left cliff value: %d\n", lBlack);
+        msleep(1000);
+        printf("Press right button to accept, left button to reject.\n");
+        while(!right_button() && !left_button()) {}
+        if (right_button()) 
+        {
+            accept = 1;
+        }
+    }
+    
+    msleep(1000);
+    accept = 0;
+    while (!accept) 
+    {
+        printf("\n\n------------------------\n\nMove RIGHT cliff sensor over black area of board.\nPress right button to set.\n");
+        while (!right_button()) {}
+        rBlack = lCliff();
+        printf("Right cliff value: %d\n", rBlack);
         msleep(1000);
         printf("Press right button to accept, left button to reject.\n");
         while(!right_button() && !left_button()) {}
@@ -189,4 +276,17 @@ void create_setup_cliff()
     }
     
     printf("\n\nCliff setup done!\n");
+}
+
+void create_skip_cliff() {
+	//Test function to quickly set up cliff sensors
+    lfWhite = 2923;
+    lWhite = 2894;
+    rfWhite = 2865;
+    rWhite = 2907;
+    
+    lfBlack = 1444;
+    rfBlack = 1044;
+    lBlack = 1166;
+    rBlack = 1166;
 }
