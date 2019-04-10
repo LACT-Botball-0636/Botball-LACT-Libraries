160fdf30dd1c7dc2b9f97177bef5700253c55b5d
Jonathan Liu <liu.z.jonathan@gmail.com>
2019-04-09 20:00:03 -0700

    Improved line follow and gyro driving

--- createDriveBetter.c
+++ createDriveBetter.c
@@ -1,7 +1,7 @@
 #include "createDrive.h"
 #include <math.h>
 /**
- * create_line_follow_center makes the create line follow by using the built-in front cliff sensors. 
+ * create_line_follow_center / *_dist makes the create line follow by using the built-in front cliff sensors. 
  * create_setup_cliff must be run before create_line_follow_center will work
  * Requires following to be defined:
  * lfBlack and lfWhite are the readings for the left front cliff sensor on the create
@@ -9,30 +9,35 @@
  * dist: distance to travel in centimeters
  * speed: speed to drive at on a range from 0-1000
  */
-void create_line_follow_center(int dist, int speed)
+void create_line_follow_center(int speed) 
 {
-  //create_line_follow_center makes the create line follow for "dist" centimeters at "speed" speed
-  
-  dist*=10; //Converts centimeters into millimeters
-  set_create_distance(0); //Clears create position counter
-  create_drive_straight(speed); //Initializes movement
-  while(abs(get_create_distance())<abs(dist)) //while distance has not been achieved, perform the following:
-  {
-    if(lfCliff() < (lfBlack+lfWhite)/2)				  //if the left tophat sensor sees black
+	if(lfCliff() < (lfBlack+lfWhite)/2)				  //if the left tophat sensor sees black
     {
-      create_drive_direct(speed*.75,speed*1.25); //drive to the left
+        create_drive_direct(speed*.75,speed*1.25); //drive to the left
     }
     else if(rfCliff() < (rfBlack+rfWhite)/2)		  //if the right tophat sensor sees black
     {
-      create_drive_direct(speed*1.25,speed*.75); //drive to the right
+        create_drive_direct(speed*1.25,speed*.75); //drive to the right
     }
     else									  //if none of the tophat sensors see black
     {
-      create_drive_straight(speed);			  //drive straight forward
+        create_drive_straight(speed);			  //drive straight forward
     }
-    msleep(5);
-  }
-  create_stop();							  //stop the create once distance has been achieved
+}
+
+void create_line_follow_center_dist(int dist, int speed)
+{
+    //create_line_follow_center makes the create line follow for "dist" centimeters at "speed" speed
+
+    dist*=10; //Converts centimeters into millimeters
+    set_create_distance(0); //Clears create position counter
+    create_drive_straight(speed); //Initializes movement
+    while(abs(get_create_distance())<abs(dist)) //while distance has not been achieved, perform the following:
+    {
+        create_line_follow_center(speed);
+        msleep(5);
+    }
+    create_stop();							  //stop the create once distance has been achieved
 }
 
 /**
@@ -93,14 +98,14 @@ void create_line_follow_right(int dist, int speed, int side) {
  */
 void create_forward(int dist, int speed)
 {
-  dist*=10; //Converts centimeters into millimeters
-  set_create_distance(0); //Clears create position counter
-  create_drive_straight(speed); //Initializes movement
-  while(get_create_distance()<dist) //Waits until distance traveled reaches distance
-  {
-    msleep(1);
-  }
-  create_stop(); //Stops movement
+    dist*=10; //Converts centimeters into millimeters
+    set_create_distance(0); //Clears create position counter
+    create_drive_straight(speed); //Initializes movement
+    while(get_create_distance()<dist) //Waits until distance traveled reaches distance
+    {
+        msleep(1);
+    }
+    create_stop(); //Stops movement
 }
 /**
  * create_backward makes the create go backward straight. 
@@ -110,14 +115,14 @@ void create_forward(int dist, int speed)
  */
 void create_backward(int dist, int speed)
 {
-  dist*=10; //Converts centimeters into millimeters
-  set_create_distance(0); //Clears create position counter
-  create_drive_straight(-speed); //Initializes movement 
-  while(get_create_distance()>-dist) //Waits until distance traveled reaches distance
-  {
-    msleep(1);
-  }
-  create_stop(); //Stops movement
+    dist*=10; //Converts centimeters into millimeters
+    set_create_distance(0); //Clears create position counter
+    create_drive_straight(-speed); //Initializes movement 
+    while(get_create_distance()>-dist) //Waits until distance traveled reaches distance
+    {
+        msleep(1);
+    }
+    create_stop(); //Stops movement
 }
 /**
  * create_left makes the create turn left in place. 
@@ -127,13 +132,13 @@ void create_backward(int dist, int speed)
  */
 void create_left(int degrees, int speed)
 {
-  set_create_total_angle(0); //Clears create angle counter
-  create_drive_direct(-speed,speed); //Initializes movement
-  while(get_create_total_angle()<degrees) //Waits until angle turned reaches degrees
-  {
-    msleep(5);
-  }
-  create_stop(); //Stops movement
+    set_create_total_angle(0); //Clears create angle counter
+    create_drive_direct(-speed,speed); //Initializes movement
+    while(get_create_total_angle()<degrees) //Waits until angle turned reaches degrees
+    {
+        msleep(5);
+    }
+    create_stop(); //Stops movement
 }
 /**
  * create_right makes the create turn right in place. 
@@ -143,59 +148,96 @@ void create_left(int degrees, int speed)
  */
 void create_right(int degrees, int speed)
 {
-  set_create_total_angle(0); //Clears create angle counter
-  create_drive_direct(speed,-speed); //Initializes movement
-  while(get_create_total_angle()>-degrees) //Waits until angle turned reaches degrees
-  {
-    msleep(5);
-  }
-  create_stop(); //Stops movement
+    set_create_total_angle(0); //Clears create angle counter
+    create_drive_direct(speed,-speed); //Initializes movement
+    while(get_create_total_angle()>-degrees) //Waits until angle turned reaches degrees
+    {
+        msleep(5);
+    }
+    create_stop(); //Stops movement
 }
 
 /**
- * create_forward_gyro makes the create go forward straight using the gyrometer 
+ * create_forward_gyro / *_dist makes the create go forward straight using the gyrometer 
  * Requires GYRO and GYRO_SENS to be defined
  * dist: distance to travel in centimeters
  * speed: speed to drive at on a range from 0-1000
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * NOTE:
+ * To gyro drive until a condition is met, model what happens in create_forward_gyro_dist,
+ * define a `double offset = 0;` and then continually set `offset = create_forward_gyro(speed, offset)`
+ * within the loop.
  */
-void create_forward_gyro(float dist, int speed) {
-  float left_speed = speed;
-  float right_speed = speed;
-  double offset = 0;
-  gyro_x(); gyro_y(); gyro_z();
-  set_create_distance(0);
-  while(get_create_distance() < dist) {
+
+double create_forward_gyro(int speed, double offset) 
+{
+    float left_speed = speed;
+    float right_speed = speed;
+    gyro_x(); gyro_y(); gyro_z();
     double val = ((double) {(abs(GYRO) == 1 ? gyro_x() : (abs(GYRO) == 2 ? gyro_y() : gyro_z()))} - gyro_dev) * -1;
     create_drive_direct(left_speed, right_speed);
     offset += val;
     left_speed = speed - ((double){offset} / GYRO_SENS);
     right_speed = speed + ((double){offset} / GYRO_SENS);
-    msleep(40);
-  }
+    msleep(5);
+    return offset;
+}
+
+void create_forward_gyro_dist(int dist, int speed) 
+{
+    double offset = 0;
+    gyro_x(); gyro_y(); gyro_z();
+    set_create_distance(0);
+    dist*=10;
+    while(abs(get_create_distance()) < abs(dist)) 
+    {
+        offset = create_forward_gyro(speed, offset);
+    }
+    create_stop();
+}
+
+/**
+ * create_square_up_gyro physically squares up the create by running create_forward_gyro for a specified amount of time
+ * Requires GYRO and GYRO_SENS to be defined
+ * ms: time to move in seconds
+ * speed: speed to drive at on a range from 0-1000
+ */
+void create_square_up_gyro(int ms, int speed) 
+{
+	double offset = 0;
+    int time = 0;
+    while (time < ms) 
+    {
+    	offset = create_forward_gyro(speed, offset);
+    	msleep(1);
+        time++;
+    }
 }
 
 /**
  * calc_dev calibrates the gyrometer
  * Requires gyro_dev to be a global variable
  */
-void calc_dev() {
-  printf("please keep robot still for 6 seconds\n press r_button when ready\n");
-  while (!right_button()) msleep(50);
-  printf("calculating...\n");
-  int time = 6000;
-  int interval = 80;
-  double sum = 0;
-  double i;
-  gyro_x(); gyro_y(); gyro_z();
-  for (i = 0; i < time / interval; ++i) {
-    // determine gyro value based on wallaby orientation
-    double val = (double) {(abs(GYRO) == 1 ? gyro_x() : (abs(GYRO) == 2 ? gyro_y() : gyro_z()))};
-    sum += val;
-    msleep(interval);
-  }
-  gyro_dev = sum / i;
-  printf("average deviation of %d \n", (int) {gyro_dev});
-  printf("gyro calib done\n");
+void calc_dev() 
+{
+    printf("please keep robot still for 6 seconds\n press r_button when ready\n");
+    while (!right_button()) msleep(50);
+    printf("calculating...\n");
+    int time = 6000;
+    int interval = 80;
+    double sum = 0;
+    double i;
+    gyro_x(); gyro_y(); gyro_z();
+    for (i = 0; i < time / interval; ++i) 
+    {
+        // determine gyro value based on wallaby orientation
+        double val = (double) {(abs(GYRO) == 1 ? gyro_x() : (abs(GYRO) == 2 ? gyro_y() : gyro_z()))};
+        sum += val;
+        msleep(interval);
+    }
+    gyro_dev = sum / i;
+    printf("average deviation of %d \n", (int) {gyro_dev});
+    printf("gyro calib done\n");
 }
 
 /**
@@ -240,7 +282,7 @@ void create_setup_cliff()
             accept = 1;
         }
     }
-    
+
     msleep(1000);
     accept = 0;
     while (!accept) 
@@ -257,7 +299,7 @@ void create_setup_cliff()
             accept = 1;
         }
     }
-    
+
     msleep(1000);
     accept = 0;
     while (!accept) 
@@ -274,19 +316,13 @@ void create_setup_cliff()
             accept = 1;
         }
     }
-    
+
     printf("\n\nCliff setup done!\n");
 }
 
-void create_skip_cliff() {
-	//Test function to quickly set up cliff sensors
-    lfWhite = 2923;
-    lWhite = 2894;
-    rfWhite = 2865;
-    rWhite = 2907;
-    
-    lfBlack = 1444;
-    rfBlack = 1044;
-    lBlack = 1166;
-    rBlack = 1166;
+void create_skip_cliff() 
+{
+    //Test function to quickly set up cliff sensors
+    lfWhite = lWhite = rfWhite = rWhite = 2900;
+    lfBlack = lBlack = rfBlack = rBlack = 1200;
 }
